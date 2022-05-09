c09aa368fa1befefa957e63b9b7c7f37890f70f8
MalteHei <malte.hei99@gmail.com>
2022-05-09 11:12:45 +0200

    add verbosity

--- src/logger.ts
+++ src/logger.ts
@@ -1,17 +1,20 @@
+import { EXTENSION_NAME } from "./extension";
+
+
 export class Logger {
 	public static DEBUG = false;
 
 	public static debug(...msg: any[]): void {
 		if (this.DEBUG) {
-			console.log(...msg);
+			console.log(`[${EXTENSION_NAME}]`, '[DEBUG]', ...msg);
 		}
 	}
 
 	public static log(...msg: any[]): void {
-		console.log(...msg);
+		console.log(`[${EXTENSION_NAME}]`, '[LOG]', ...msg);
 	}
 
 	public static error(...msg: any[]): void {
-		console.error(...msg);
+		console.error(`[${EXTENSION_NAME}]`, '[ERROR]', ...msg);
 	}
 }
